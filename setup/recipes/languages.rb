6d4bc940648fc9dda7b4238ff5e17154e607e671
ta1kt0me <p.wadachi@gmail.com>
2017-02-20 01:08:10 +0900

    Not ruby, just nodejs

--- setup/recipes/languages.rb
+++ setup/recipes/languages.rb
@@ -2,6 +2,6 @@ include_recipe "rbenv::user"
 
 execute "Install nodejs" do
   user node[:user]
-  command "nodebrew install-binary #{node[:nodejs][:version]} && nodebrew use #{node[:ruby][:version]}"
+  command "nodebrew install-binary #{node[:nodejs][:version]} && nodebrew use #{node[:nodejs][:version]}"
   not_if "[[ $(nodebrew ls | grep current) =~ #{node[:nodejs][:version]} ]]"
 end
