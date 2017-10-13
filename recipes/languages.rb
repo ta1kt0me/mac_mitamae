0846f99421ba8b59439201be1b7f60f8c18d95b0
ta1kt0me <p.wadachi@gmail.com>
2017-10-13 17:33:04 +0900

    Follow nodebrew official document

--- recipes/languages.rb
+++ recipes/languages.rb
@@ -1,7 +1,14 @@
 include_recipe "rbenv::user"
 
-execute "Install nodejs" do
+execute "Install nodebrew" do
   user node[:user]
-  command "nodebrew install-binary #{node[:nodejs][:version]} && nodebrew use #{node[:nodejs][:version]}"
-  not_if "[[ $(nodebrew ls | grep current) =~ #{node[:nodejs][:version]} ]]"
+  command "curl -L git.io/nodebrew | perl - setup"
+  not_if "[[ -e $HOME/.nodebrew/current/bin/nodebrew ]]"
+end
+
+execute "Install nodejs" do
+  nodebrew = "$HOME/.nodebrew/current/bin/nodebrew"
+  user node[:user]
+  command "#{nodebrew} install-binary #{node[:nodejs][:version]} && #{nodebrew} use #{node[:nodejs][:version]}"
+  not_if "[[ $(#{nodebrew} ls | grep current) =~ #{node[:nodejs][:version]} ]]"
 end
