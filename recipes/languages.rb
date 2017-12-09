410bd5a7dc2dd79127033809cc50c0bb5c3a0882
ta1kt0me <p.wadachi@gmail.com>
2017-12-10 01:57:01 +0900

    Always update nodebrew

--- recipes/languages.rb
+++ recipes/languages.rb
@@ -3,12 +3,11 @@ include_recipe "rbenv::user"
 execute "Install nodebrew" do
   user node[:user]
   command "curl -L git.io/nodebrew | perl - setup"
-  not_if "[[ -e $HOME/.nodebrew/current/bin/nodebrew ]]"
 end
 
 execute "Install nodejs" do
   nodebrew = "$HOME/.nodebrew/current/bin/nodebrew"
   user node[:user]
   command "#{nodebrew} install-binary #{node[:nodejs][:version]} && #{nodebrew} use #{node[:nodejs][:version]}"
-  only_if "[[ -z $(#{nodebrew} ls | grep #{node[:nodejs][:version]} ]]"
+  not_if "[[ ! -z $(#{nodebrew} ls | grep #{node[:nodejs][:version]} ]]"
 end
