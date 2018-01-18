b7eb33fd56b2c12a86b72314ffc0e5d6949bd55a
ta1kt0me <p.wadachi@gmail.com>
2018-01-18 11:27:59 +0900

    OMG

--- recipes/languages.rb
+++ recipes/languages.rb
@@ -9,5 +9,5 @@ execute "Install nodejs" do
   nodebrew = "$HOME/.nodebrew/current/bin/nodebrew"
   user node[:user]
   command "#{nodebrew} install-binary #{node[:nodejs][:version]} && #{nodebrew} use #{node[:nodejs][:version]}"
-  not_if "[[ ! -z $(#{nodebrew} ls | grep #{node[:nodejs][:version]} ]]"
+  not_if "[[ ! -z $(#{nodebrew} ls | grep #{node[:nodejs][:version]}) ]]"
 end
