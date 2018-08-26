bb85946f0c4c3f260b4f2bfdf0381bcc1231b88f
ta1kt0me <p.wadachi@gmail.com>
2018-08-26 23:24:21 +0900

    Setup rust

--- recipes/languages.rb
+++ recipes/languages.rb
@@ -101,3 +101,40 @@ node[:go_packages].each do |package|
     command "GOPATH=#{GO_PATH} #{GO_BIN}/go-get-release github.com/#{package}"
   end
 end
+
+# rust
+execute "Install rsvm" do
+  user node[:user]
+  command "curl -L https://raw.github.com/sdepold/rsvm/master/install.sh | sh"
+end
+
+RSVM_HOME = ENV["HOME"] + "/.rsvm"
+execute "Link fish-config for rsvm" do
+  user node[:user]
+  command "ln -s #{RSVM_HOME}/rsvm.fish ${HOME}/.config/fish/functions"
+  not_if "test -e ${HOME}/.config/fish/functions/rsvm.fish"
+end
+
+node[:rust][:versions].each do |version|
+  execute "Install rust #{version}" do
+    user node[:user]
+    cwd ENV["HOME"]
+    command ". #{RSVM_HOME}/rsvm.sh && rsvm install #{version}"
+    not_if ". #{RSVM_HOME}/rsvm.sh && test -n \"$(rsvm ls | grep #{version})\""
+  end
+end
+
+node[:rust][:global].tap do |version|
+  execute "Set rsvm use #{version}" do
+    user node[:user]
+    command ". #{RSVM_HOME}/rsvm.sh && rsvm use #{version}"
+  end
+end
+
+node[:rust_packages].each do |package|
+  execute "Install rust package: #{package}" do
+    user node[:user]
+    command ". #{RSVM_HOME}/rsvm.sh && cargo install #{package}"
+    not_if ". #{RSVM_HOME}/rsvm.sh && test -n \"$(cargo install --list | grep #{package})\""
+  end
+end
