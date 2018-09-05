8ee13d4241daa1725fc8d490f72ffea2e4564ec6
ta1kt0me <p.wadachi@gmail.com>
2018-09-05 21:38:33 +0900

    Exec as root

--- recipes/add_apt_repository.rb
+++ recipes/add_apt_repository.rb
@@ -1,6 +1,5 @@
 node[:apt_repositories].each do |repo|
   execute "Install #{repo}" do
-    user node[:user]
     command "add-apt-repository #{repo}"
     not_if "test -n \"$(apt-cache policy | grep #{repo.split(':')[1]})\""
   end
