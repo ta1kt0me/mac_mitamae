9a8dc8f659f8899db4e50bb5b8ef40f970e46ab5
ta1kt0me <p.wadachi@gmail.com>
2018-09-05 20:46:02 +0900

    Follow vimrc setting

--- recipes/symlink.rb
+++ recipes/symlink.rb
@@ -1,6 +1,12 @@
-execute "Create ghq symlink" do
-  ghq_bin = ENV["HOME"] + "/src/bin/ghq"
-  user node[:user]
-  command "ln -s #{ghq_bin} /usr/local/bin/ghq"
-  not_if "test -L /usr/local/bin/ghq"
+commands = [
+  { command: "ghq",  actual: ENV["HOME"] + "/src/bin/ghq" },
+  { command: "fish", actual: "/usr/bin/fish" },
+]
+
+commands.each do |command|
+  execute "Create #{command[:command]} symlink" do
+    user node[:user]
+    command "ln -s #{command[:actual]} /usr/local/bin/#{command[:command]}"
+    not_if "test -L /usr/local/bin/#{command[:command]}"
+  end
 end
