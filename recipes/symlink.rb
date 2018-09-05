98b57502c948e90deddf285d22e4854204171b25
ta1kt0me <p.wadachi@gmail.com>
2018-09-05 20:58:46 +0900

    Correct user

--- recipes/symlink.rb
+++ recipes/symlink.rb
@@ -5,7 +5,6 @@ commands = [
 
 commands.each do |command|
   execute "Create #{command[:command]} symlink" do
-    user node[:user]
     command "ln -s #{command[:actual]} /usr/local/bin/#{command[:command]}"
     not_if "test -L /usr/local/bin/#{command[:command]}"
   end
