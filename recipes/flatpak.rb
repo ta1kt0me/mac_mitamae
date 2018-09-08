037fdf6583f1b5deecb123ae9437e577c2a28272
ta1kt0me <p.wadachi@gmail.com>
2018-09-08 23:13:19 +0900

    Install podcast client

--- recipes/flatpak.rb
+++ recipes/flatpak.rb
@@ -6,6 +6,18 @@ end
 
 execute "Install vocal" do
   user node[:user]
-  command "flatpak install flathub com.github.needleandthread.vocal"
-  not_if "test -n \"$( flatpak list | grep vocal)\""
+  command "flatpak install -y flathub com.github.needleandthread.vocal"
+  not_if "test -n \"$(flatpak list | grep vocal)\""
+end
+
+execute "Add nuvola repository" do
+  user node[:user]
+  command "flatpak remote-add --if-not-exists nuvola https://dl.tiliado.eu/flatpak/nuvola.flatpakrepo"
+  not_if "test -n \"$(flatpak remotes | grep nuvola)\""
+end
+
+execute "Install pocket casts" do
+  user node[:user]
+  command "flatpak install -y nuvola eu.tiliado.NuvolaAppPocketCasts"
+  not_if "test -n \"$(flatpak list | grep PocketCasts)\""
 end
