a66d9a9691c13d39cb7aade514a0d982d3f8096c
ta1kt0me <p.wadachi@gmail.com>
2018-11-10 06:29:38 +0900

    Comment out until ppa is available in cosmic

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -2,8 +2,8 @@ execute "apt-key add docker" do
   command "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -"
 end
 
-execute "apt-apt-repository docker" do
-  command "add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\""
-end
+# execute "apt-apt-repository docker" do
+#   command "add-apt-repository \"deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\""
+# end
 
 package "docker-ce"
