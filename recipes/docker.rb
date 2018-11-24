3a57672274de5e3a50f57f08ed83a8600618e5ba
ta1kt0me <p.wadachi@gmail.com>
2018-11-25 01:05:58 +0900

    Use bionic repo in ubuntu 18.10

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -2,8 +2,10 @@ execute "apt-key add docker" do
   command "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -"
 end
 
-# execute "apt-apt-repository docker" do
-#   command "add-apt-repository \"deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\""
-# end
+execute "add-apt-repository docker" do
+  # ubuntu 18.10
+  command "add-apt-repository \"deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu bionic stable\""
+  not_if "test -n \"$(apt-cache policy | grep 'download.docker.com/linux/ubuntu bionic')\""
+end
 
 package "docker-ce"
