7faa8790797d2ee6aa1454a0562005360a318e50
ta1kt0me <p.wadachi@gmail.com>
2019-02-04 00:04:10 +0900

    Update homebrew v2

--- recipes/linuxbrew.rb
+++ recipes/linuxbrew.rb
@@ -1,4 +1,18 @@
-git "#{ENV['HOME']}/.linuxbrew" do
+LINUXBREW_PREFIX = "$HOME/.linuxbrew".freeze
+
+execute "Make directory for linuxbrew" do
   user node[:user]
-  repository "git@github.com:Linuxbrew/brew.git"
+  command "mkdir -p #{LINUXBREW_PREFIX}"
+  not_if "test -d #{LINUXBREW_PREFIX}"
+end
+
+git "#{LINUXBREW_PREFIX}/Homebrew" do
+  user node[:user]
+  repository "git@github.com:Homebrew/brew.git"
+end
+
+execute "Link brew command" do
+  user node[:user]
+  command "ln -s #{LINUXBREW_PREFIX}/Homebrew/bin #{LINUXBREW_PREFIX}/bin"
+  not_if "test -L #{LINUXBREW_PREFIX}/bin"
 end
