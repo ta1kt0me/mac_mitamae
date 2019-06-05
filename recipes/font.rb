bef117a69f361db44737fe12de70094f8de9a840
ta1kt0me <p.wadachi@gmail.com>
2019-06-05 23:51:51 +0900

    Install kinto font

--- recipes/font.rb
+++ recipes/font.rb
@@ -2,3 +2,8 @@ execute "Install font for powerline" do
   user node[:user]
   command 'cd "$(/usr/local/bin/ghq list -p | grep powerline/fonts)" && ./install.sh'
 end
+
+execute "Install kinto font" do
+  user node[:user]
+  command 'cd "$(/usr/local/bin/ghq list -p | grep ookamiinc/kinto)" && find "Kinto Sans" -name "*.[ot]tf" -or -name "*.pcf.gz" -type f | xargs -I% cp "%" ${HOME}/.local/share/fonts/ && fc-cache -f ${HOME}/.local/share/fonts/'
+end
