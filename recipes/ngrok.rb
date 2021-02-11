daed68c11bf2424bc93935cbbc1dd12fdbdbf41e
ta1kt0me <p.wadachi@gmail.com>
2021-02-11 23:03:13 +0900

    enable ngrok recipe on mac

--- recipes/ngrok.rb
+++ recipes/ngrok.rb
@@ -3,24 +3,28 @@ def home_dir
 end
 
 home_path = "#{home_dir}/" + node[:user]
+bin_path = home_path + "/.local/bin"
+
+ngrok_url = \
+  node[:platform] == "darwin" ? \
+    "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip" : \
+    "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip"
 
 execute "Make directory for local_bin" do
   user node[:user]
-  command "mkdir -p #{home_path}/.local/bin"
-  not_if "test -d #{home_path}/.local/bin"
+  command "mkdir -p #{bin_path}"
+  not_if "test -d #{bin_path}"
 end
 
-
-# downloadリンクがubuntu
 # jar コマンドがある前提
 execute "Download ngrok" do
   user node[:user]
-  cwd "#{home_path}/.local/bin"
-  command "curl -sL -o - https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip | jar -xvf /dev/stdin && chmod +x ngrok"
-  not_if "test -e #{home_path}/.local/bin/ngrok"
+  cwd "#{bin_path}"
+  command "curl -sL -o - #{ngrok_url} | jar -xvf /dev/stdin && chmod +x ngrok"
+  not_if "test -e #{bin_path}/ngrok"
 end
 
 execute "Update ngrok" do
   user node[:user]
-  command "#{home_path}/.local/bin/ngrok update"
+  command "#{bin_path}/ngrok update"
 end
