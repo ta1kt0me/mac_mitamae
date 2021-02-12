561ddd39026a29b92fdfa0c85f09527da0ac60b7
ta1kt0me <p.wadachi@gmail.com>
2021-02-12 21:56:26 +0900

    use full ghq path

--- recipes/font.rb
+++ recipes/font.rb
@@ -1,9 +1,17 @@
+def home_dir
+  node[:platform] == "darwin" ? "/Users" : "/home"
+end
+
+home_path = "#{home_dir}/" + node[:user]
+go_global_bin = "#{home_path}/go/#{node[:goenv][:global]}/bin"
+ghq_cli = "#{go_global_bin}/ghq"
+
 execute "Install font for powerline" do
   user node[:user]
-  command 'cd "$(/usr/local/bin/ghq list -p | grep powerline/fonts)" && ./install.sh'
+  command "cd \"$(#{ghq_cli} list -p | grep powerline/fonts)\" && ./install.sh"
 end
 
 execute "Install kinto font" do
   user node[:user]
-  command 'cd "$(/usr/local/bin/ghq list -p | grep ookamiinc/kinto)" && find "Kinto Sans" -name "*.[ot]tf" -or -name "*.pcf.gz" -type f | xargs -I% cp "%" ${HOME}/.local/share/fonts/ && fc-cache -f ${HOME}/.local/share/fonts/'
+  command "cd \"$(#{ghq_cli} list -p | grep ookamiinc/kinto)\" && find 'Kinto Sans' -name '*.[ot]tf' -or -name '*.pcf.gz' -type f | xargs -I% cp '%' ${HOME}/.local/share/fonts/ && fc-cache -f ${HOME}/.local/share/fonts/"
 end
