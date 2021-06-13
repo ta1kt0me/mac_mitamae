41a022e5fb9459631f2f03ebc98d5e793dfe829d
ta1kt0me <p.wadachi@gmail.com>
2021-06-13 21:42:57 +0900

    remove package installation via homebrew on linux

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -12,13 +12,4 @@ if node[:platform] == "ubuntu"
   node[:apt_packages].each do |item|
     package item
   end
-
-  node[:linuxbrew_packages].each do |item|
-    LINUX_BREW = "/home/#{node[:user]}/.linuxbrew/bin/brew"
-    execute "Install #{item}" do
-      user node[:user]
-      command "#{LINUX_BREW} install #{item}"
-      not_if "test -n \"$(#{LINUX_BREW} list | grep #{item})\""
-    end
-  end
 end
