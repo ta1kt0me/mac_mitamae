ece23092b579f6b4d3dda3dceefb8f9c9e695428
ta1kt0me <p.wadachi@gmail.com>
2021-08-06 00:39:02 +0900

    stop using docker-compose via pip

--- recipes/usr_local_link.rb
+++ recipes/usr_local_link.rb
@@ -1,16 +1,16 @@
-def home_dir
-  node[:platform] == "darwin" ? "/Users" : "/home"
-end
-
-links = [
-  {
-    from: "/usr/local/bin/docker-compose",
-    to: "#{home_dir}/#{node[:user]}/.pyenv/shims/docker-compose"
-  }
-]
-
-links.each do |link|
-  link link[:from] do
-    to link[:to]
-  end
-end
+# def home_dir
+#   node[:platform] == "darwin" ? "/Users" : "/home"
+# end
+# 
+# links = [
+#   {
+#     from: "/usr/local/bin/docker-compose",
+#     to: "#{home_dir}/#{node[:user]}/.pyenv/shims/docker-compose"
+#   }
+# ]
+# 
+# links.each do |link|
+#   link link[:from] do
+#     to link[:to]
+#   end
+# end
