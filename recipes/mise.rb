3f06bb92ad48333b38c5adb2ed89bbbf9dff3bf8
ta1kt0me <1412864+ta1kt0me@users.noreply.github.com>
2025-11-04 09:43:50 +0900

    refactor mise

--- recipes/mise.rb
+++ recipes/mise.rb
@@ -5,33 +5,25 @@ HOME_DIR = DirectoryHelper.home(node) + "/" + node[:user]
 execute "Install mise" do
   user node[:user]
   command "curl https://mise.run | sh"
-  not_if "test -e #{HOME_DIR}/.local/bin/mise"
 end
 
-# node
-node[:mise][:node][:versions].each do |version|
-  execute "Install node@#{version}}" do
+node[:mise][:packages].each do |package|
+  execute "Install #{package} via mise" do
     user node[:user]
-    command "#{HOME_DIR}/.local/bin/mise install -q node@#{version}"
-    only_if "test -z $(#{HOME_DIR}/.local/bin/mise ls node | grep #{version})"
+    command "#{HOME_DIR}/.local/bin/mise install #{package}"
+    only_if "test -z $(#{HOME_DIR}/.local/bin/mise ls #{package.split("@").first} | grep #{package.split("@").last})"
   end
 end
 
-execute "Install node@#{node[:mise][:node][:use]}}" do
-  user node[:user]
-  command "#{HOME_DIR}/.local/bin/mise use node@#{node[:mise][:node][:use]}"
-end
-
-# ruby
-node[:mise][:ruby][:versions].each do |version|
-  execute "Install ruby@#{version}}" do
+node[:mise][:plugins].each do |plugin|
+  execute "Install #{plugin} via mise" do
     user node[:user]
-    command "#{HOME_DIR}/.local/bin/mise install -q ruby@#{version}"
-    only_if "test -z $(#{HOME_DIR}/.local/bin/mise ls ruby | grep #{version})"
+    command "#{HOME_DIR}/.local/bin/mise plugins install #{plugin}"
+    only_if "test -z $(#{HOME_DIR}/.local/bin/mise ls #{plugin})"
   end
-end
 
-execute "Install ruby@#{node[:mise][:ruby][:use]}}" do
-  user node[:user]
-  command "#{HOME_DIR}/.local/bin/mise use ruby@#{node[:mise][:ruby][:use]}"
+  execute "Update #{plugin} via mise" do
+    user node[:user]
+    command "#{HOME_DIR}/.local/bin/mise plugins update #{plugin}"
+  end
 end
