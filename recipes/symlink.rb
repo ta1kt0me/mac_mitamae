21f5ab141126e22ac2e8776b61c5c33fb2f3c87f
ta1kt0me <1412864+ta1kt0me@users.noreply.github.com>
2025-11-18 00:44:01 +0900

    simplify

--- recipes/symlink.rb
+++ recipes/symlink.rb
@@ -1,8 +1,6 @@
 include_recipe 'directory_helper'
 
-home_dir = DirectoryHelper.home(node)
 commands = [
-  { command: "ghq",  actual: "#{home_dir}/#{node[:user]}/go/#{node[:goenv][:global]}/bin/ghq" },
   { command: "fish", actual: "/usr/bin/fish" }
 ]
 
