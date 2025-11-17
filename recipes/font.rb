1dffacd2d1d0202e1ce5cf3155950b5b640c3f52
ta1kt0me <1412864+ta1kt0me@users.noreply.github.com>
2025-11-18 00:40:19 +0900

    simplify

--- recipes/font.rb
+++ recipes/font.rb
@@ -1,16 +1,9 @@
-include_recipe 'directory_helper'
-
-home_dir = DirectoryHelper.home(node)
-home_path = "#{home_dir}/" + node[:user]
-go_global_bin = "#{home_path}/go/#{node[:goenv][:global]}/bin"
-ghq_cli = "#{go_global_bin}/ghq"
-
 execute "Install font for powerline" do
   user node[:user]
-  command "cd \"$(#{ghq_cli} list -p | grep powerline/fonts)\" && ./install.sh"
+  command "cd \"$(ghq list -p | grep powerline/fonts)\" && ./install.sh"
 end
 
 execute "Install kinto font" do
   user node[:user]
-  command "cd \"$(#{ghq_cli} list -p | grep ookamiinc/kinto)\" && find 'Kinto Sans' -name '*.[ot]tf' -or -name '*.pcf.gz' -type f | xargs -I% cp '%' ${HOME}/.local/share/fonts/ && fc-cache -f ${HOME}/.local/share/fonts/"
+  command "cd \"$(ghq list -p | grep ookamiinc/kinto)\" && find 'Kinto Sans' -name '*.[ot]tf' -or -name '*.pcf.gz' -type f | xargs -I% cp '%' ${HOME}/.local/share/fonts/ && fc-cache -f ${HOME}/.local/share/fonts/"
 end
