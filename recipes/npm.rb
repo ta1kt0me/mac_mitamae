c117eb951aacb423adcd7914bbe7ef0177ebcb2c
ta1kt0me <1412864+ta1kt0me@users.noreply.github.com>
2025-11-18 00:38:33 +0900

    remove cleanup languages

--- recipes/npm.rb
+++
@@ -1,6 +0,0 @@
-node[:npm].each do |package|
-  execute "Install #{package}" do
-    user node[:user]
-    command "PATH=$HOME/.nodebrew/current/bin:$PATH npm install -g #{package}"
-  end
-end
--- recipes/pip.rb
+++
@@ -1,6 +0,0 @@
-node[:pip].each do |tool|
-  pip tool do
-    user node[:user]
-    pip_binary "#{node[:platform] == "darwin" ? "/Users" : "/home"}/#{node[:user]}/.pyenv/shims/pip3"
-  end
-end
